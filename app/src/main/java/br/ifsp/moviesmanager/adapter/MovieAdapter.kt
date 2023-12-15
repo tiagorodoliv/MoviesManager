05599734bf3712fc5e5a9bdfdbb9cb08fe97f93e
Tiago Silas <tiago.rod.oliv@gmail.com>
2023-12-14 23:02:01 -0300

    Adicionando visualiza;'ao dos filmes na tela main

---
+++ app/src/main/java/br/ifsp/moviesmanager/adapter/MovieAdapter.kt
@@ -0,0 +1,38 @@
+package br.ifsp.moviesmanager.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import br.ifsp.moviesmanager.data.Movie
+import br.ifsp.moviesmanager.databinding.MovieCelulaBinding
+
+class MovieAdapter():
+    RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
+    private lateinit var binding: MovieCelulaBinding
+    var moviesLista = ArrayList<Movie>()
+    var moviesListaFilterable = ArrayList<Movie>()
+    fun updateList(newList: ArrayList<Movie> ){
+        moviesLista = newList
+        moviesListaFilterable = moviesLista
+        notifyDataSetChanged()
+    }
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): MovieViewHolder {
+        binding = MovieCelulaBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MovieViewHolder(binding)
+    }
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        holder.nomeVH.text = moviesLista[position].nome
+        holder.noteVH.text = moviesLista[position].note
+    }
+    override fun getItemCount(): Int {
+        return moviesLista.size
+    }
+    inner class MovieViewHolder(view:MovieCelulaBinding): RecyclerView.ViewHolder(view.root)
+    {
+        val nomeVH = view.name
+        val noteVH = view.note
+    }
+}
--- app/src/main/java/br/ifsp/moviesmanager/ui/ListMoviesFragment.kt
+++ app/src/main/java/br/ifsp/moviesmanager/ui/ListMoviesFragment.kt
@@ -5,15 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import br.ifsp.moviesmanager.R
+import br.ifsp.moviesmanager.adapter.MovieAdapter
+import br.ifsp.moviesmanager.data.Movie
 import br.ifsp.moviesmanager.databinding.FragmentListMoviesBinding
+import br.ifsp.moviesmanager.viewmodel.MovieViewModel
 
 class ListMoviesFragment : Fragment() {
 
     private var _binding: FragmentListMoviesBinding? = null
     private val binding get() = _binding!!
+    lateinit var movieAdapter: MovieAdapter
+    lateinit var viewModel: MovieViewModel
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -23,6 +34,22 @@ class ListMoviesFragment : Fragment() {
         binding.fab.setOnClickListener {
             findNavController().navigate(R.id.action_listMoviesFragment_to_cadastroFragment)
         }
-        return root
+
+        configureRecyclerView()
+
+        return binding.root
+    }
+
+    private fun configureRecyclerView() {
+        viewModel = ViewModelProvider(this).get(MovieViewModel::class.java)
+        viewModel.allMovies.observe(viewLifecycleOwner) { list ->
+            list?.let {
+                movieAdapter.updateList(list as ArrayList<Movie>)
+            }
+        }
+        val recyclerView = binding.recyclerview
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        movieAdapter = MovieAdapter()
+        recyclerView.adapter = movieAdapter
     }
 }
